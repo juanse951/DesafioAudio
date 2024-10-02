915742d754c242d2b7bf2b4157b56e34dd2cf14c
juanse951 <juanse951@gmail.com>
2024-10-02 08:51:26 -0500

    creacion de superclases, sus atributos. getter y setters

---
+++ src/modelos/Audio.java
@@ -0,0 +1,58 @@
+package modelos;
+
+public class Audio {
+    private String titulo;
+    private int duracion;
+    private int totalDeReproducciones;
+    private int meGusta;
+    private int clasificacion;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    public int getTotalDeReproducciones() {
+        return totalDeReproducciones;
+    }
+
+    public void setTotalDeReproducciones(int totalDeReproducciones) {
+        this.totalDeReproducciones = totalDeReproducciones;
+    }
+
+    public int getMeGusta() {
+        return meGusta;
+    }
+
+    public void setMeGusta(int meGusta) {
+        this.meGusta = meGusta;
+    }
+
+    public int getClasificacion() {
+        return clasificacion;
+    }
+
+    public void setClasificacion(int clasificacion) {
+        this.clasificacion = clasificacion;
+    }
+
+    public void meGusta(){
+
+    }
+
+    public void reproducir(){
+
+    }
+
+}
